df61fe3470073a30f378ddd0676447c594ce0a39
wladimirgrf <wladimir.code@gmail.com>
2023-12-08 14:01:18 -0300

    Configuring Stitches

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,8 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 
+import { getCssText } from '@/styles'
+
+
 export default function Document() {
   return (
     <Html lang="en">
@@ -7,6 +10,8 @@ export default function Document() {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet" />
+
+        <style id="stiches" dangerouslySetInnerHTML={{ __html: getCssText() }} />
       </Head>
       <body>
         <Main />
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,13 @@
+import { styled } from "@/styles"
 
+const Button = styled('button', {
+  backgroundColor: '$rocketseat'
+})
 
 
 
 export default function Home() {
   return (
-    <h1>Hello</h1>
+    <Button>Send</Button>
   )
 }
