963fb9a6e406da27921196376ae9acaa5f04820a
wladimirgrf <wladimir.code@gmail.com>
2023-12-11 17:21:05 -0300

    Estrutura da pagina de produto

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,11 +1,22 @@
 import { useRouter } from "next/router"
 
-
+import { ImageContainer, ProductContainer, ProductDetails } from "@/styles/pages/product"
 
 export default function Product() {
   const {query} = useRouter()
 
   return (
-    <h1>Product</h1>
+    <ProductContainer>
+      <ImageContainer>
+      </ImageContainer>
+
+      <ProductDetails>
+        <h1>Tshirt X</h1>
+        <span>$44,9</span>
+        <p>lorem</p>
+
+        <button>Buy</button>
+      </ProductDetails>
+    </ProductContainer>
   )
 }
