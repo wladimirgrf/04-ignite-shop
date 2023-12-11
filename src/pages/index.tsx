6820673c5ca512a594594086d03910b1fa6f42b1
wladimirgrf <wladimir.code@gmail.com>
2023-12-11 13:33:57 -0300

    Criando carrossel

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,7 @@
+import 'keen-slider/keen-slider.min.css'
+
 import Image from "next/image";
+import {useKeenSlider} from 'keen-slider/react'
 
 import { HomeContainer, Product } from "@/styles/pages/home";
 
@@ -7,9 +10,16 @@ import shirt2 from '../assets/t-shirts/2.png'
 import shirt3 from '../assets/t-shirts/3.png'
 
 export default function Home() {
+  const [sliderRef] = useKeenSlider({
+    slides: { 
+      perView: 3, 
+      spacing: 48 
+    }
+  })
+
   return (
-    <HomeContainer>
-      <Product>
+    <HomeContainer ref={sliderRef} className='keen-slider'>
+      <Product className='keen-slider__slide'>
         <Image src={shirt1} width={520} height={480} alt="" />
 
         <footer>
@@ -18,8 +28,26 @@ export default function Home() {
         </footer>
       </Product>
 
-      <Product>
+      <Product className='keen-slider__slide'>
         <Image src={shirt2} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>T-shirt Y</strong>
+          <span>$29,90</span>
+        </footer>
+      </Product>
+
+      <Product className='keen-slider__slide'>
+        <Image src={shirt3} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>T-shirt Y</strong>
+          <span>$29,90</span>
+        </footer>
+      </Product>
+
+      <Product className='keen-slider__slide'>
+        <Image src={shirt3} width={520} height={480} alt="" />
 
         <footer>
           <strong>T-shirt Y</strong>
