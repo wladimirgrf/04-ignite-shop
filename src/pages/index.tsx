dac80e03703173bda1d70c22e64eab03d84131ed
wladimirgrf <wladimir.code@gmail.com>
2023-12-11 11:43:15 -0300

    Estrutura da Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,31 @@
-import { styled } from "@/styles"
+import Image from "next/image";
 
-const Button = styled('button', {
-  backgroundColor: '$gray300'
-})
+import { HomeContainer, Product } from "@/styles/pages/home";
 
-
+import shirt1 from '../assets/t-shirts/1.png'
+import shirt2 from '../assets/t-shirts/2.png'
+import shirt3 from '../assets/t-shirts/3.png'
 
 export default function Home() {
   return (
-    <Button>Send</Button>
+    <HomeContainer>
+      <Product>
+        <Image src={shirt1} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>T-shirt X</strong>
+          <span>$29,90</span>
+        </footer>
+      </Product>
+
+      <Product>
+        <Image src={shirt2} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>T-shirt Y</strong>
+          <span>$29,90</span>
+        </footer>
+      </Product>
+    </HomeContainer>
   )
 }
