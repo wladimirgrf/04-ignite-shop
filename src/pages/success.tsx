9f3ffde753ae7def25d2dbac4681447f7321a57a
wladimirgrf <wladimir.code@gmail.com>
2023-12-12 13:30:53 -0300

    Estrutura de sucesso

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,5 +1,22 @@
+import { ImageContainer, SuccessContainer } from "@/styles/pages/success";
+import Link from "next/link";
+
 export default function Success() {
   return (
-    <h1>Success</h1>
+    <SuccessContainer>
+      <h1>Purchase made!</h1>
+
+      <ImageContainer>
+
+      </ImageContainer>
+
+      <p>
+        Woohoo <strong>Diego Fernandes</strong>, your <strong>Beyond the Limits T-shirt</strong> is already on its way to your home.
+      </p>
+
+      <Link href="/">
+        Back to catalog
+      </Link>
+    </SuccessContainer>
   )
 }
